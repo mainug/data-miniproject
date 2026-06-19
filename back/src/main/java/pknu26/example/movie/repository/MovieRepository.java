cc588666c28c94e7f639a57f22e5ce164e64a1d0
mainug <alsdnr0850@gmail.com>
2026-06-19 09:31:33 +0900

    어노테이션 수정

--- back/src/main/java/pknu26/example/movie/repository/MovieRepository.java
+++ back/src/main/java/pknu26/example/movie/repository/MovieRepository.java
@@ -4,13 +4,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import pknu26.example.movie.dto.GenreStatResponse;
 import pknu26.example.movie.entity.Movie;
 
 import java.util.List;
 
-@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     
     List<Movie> findByTitleContainingIgnoreCase(String keyword);
